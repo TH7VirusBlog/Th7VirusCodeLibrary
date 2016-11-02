1b2d4f2e2e187dbaee7459992ac87e7cd4a6c1bf
مدونة فيرس للمعلوميات <omarkamelmustafa@gmail.com>
2016-11-02 11:17:19 +0200

    Update TH7virusSpecialCode.js

--- TH7virusSpecialCode.js
+++ TH7virusSpecialCode.js
@@ -4,19 +4,17 @@
 // ---------------------------------------------------
 
 // ---------------------------------------------------
-// BLOGTOC
+// TH7VIRUS.COM
 // ---------------------------------------------------
-// BlogToc creates a clickable Table Of Contents for
+// THIS SCRIPT creates a clickable Table Of Contents for
 // Blogger Blogs.
 // It uses the JSON post feed, and create a ToC of it.
 // The ToC can be sorted by title or by date, both
 // ascending and descending, and can be filtered by
 // label.
 // ---------------------------------------------------
-// Author: Beautiful Beta
-// Url: http://beautifulbeta.blogspot.com
-// Version: 2
-// Date: 2007-04-12
+// Author: Omar Kamel Mostaf Ali 
+// Url: http://TH7VIRUS.blogspot.com
    var postTitle = new Array();     // array of posttitles
    var postUrl = new Array();       // array of posturls
    var postDate = new Array();      // array of post publish dates
